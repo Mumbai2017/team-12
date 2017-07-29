73419237095f393a307026ef79c9c2cdf2016ca7
rohitx71 <rohitsingh2359@gmail.com>
2017-07-29 15:51:06 +0530

    signup login java

--- MakeAWish/app/src/main/java/com/cfg/team12/makeawish/LoginActivity.java
+++ MakeAWish/app/src/main/java/com/cfg/team12/makeawish/LoginActivity.java
@@ -2,15 +2,29 @@ package com.cfg.team12.makeawish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import butterknife.InjectView;
 
 public class LoginActivity extends AppCompatActivity {
 
     Spinner stakeholder;
     String data[]=new String[]{"Donor","Volunteer"};
+    Button loginButton;
+
+    @InjectView(com.cfg.team12.makeawish.R.id.input_email) EditText _emailText;
+    @InjectView(com.cfg.team12.makeawish.R.id.input_password) EditText _passwordText;
+    @InjectView(com.cfg.team12.makeawish.R.id.btn_login)      Button _loginButton;
+    @InjectView(com.cfg.team12.makeawish.R.id.link_signup)
+    TextView _signupLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,23 @@ public class LoginActivity extends AppCompatActivity {
 
         stakeholder=(Spinner)findViewById(R.id.spinner_select);
         stakeholder.setAdapter(arrayAdapter);
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+            login();
+               // Toast.makeText(mContext, "Hello moto!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void login() {
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
 
     }
 }
--- MakeAWish/app/src/main/java/com/cfg/team12/makeawish/SignUpActivity.java
+++ MakeAWish/app/src/main/java/com/cfg/team12/makeawish/SignUpActivity.java
@@ -2,12 +2,43 @@ package com.cfg.team12.makeawish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.InjectView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    @InjectView(com.cfg.team12.makeawish.R.id.input_email)
+    EditText email;
+    @InjectView(com.cfg.team12.makeawish.R.id.input_password) EditText password;
+    @InjectView(R.id.input_name) EditText name;
+    @InjectView(R.id.input_aadhar)EditText aadhar;
+    @InjectView(R.id.input_city)EditText location;
+
+
+    @InjectView(R.id.btn_signup)
+    Button signUpButton;
+    @InjectView(com.cfg.team12.makeawish.R.id.link_signup)
+    TextView _signupLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+    }
+
+    private void register() {
+        String email2 = email.getText().toString();
+        String password2 = password.getText().toString();
     }
 }
