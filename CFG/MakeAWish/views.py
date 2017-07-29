95a8c44dfbd18d3a366aefa789710659f48c84f3
rajatbhagat <rajat.bhagat@gmail.com>
2017-07-29 20:56:07 +0530

    Created views

--- CFG/MakeAWish/views.py
+++ CFG/MakeAWish/views.py
@@ -1,5 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from MakeAWish.models import ChildProfile, Doctor, Volunteer, Donor, Transaction
 
 # Create your views here.
+
+def index(reqeust):
+    return HttpResponse("This is the index page")
+
+
 
