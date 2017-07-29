2a192f6e6d11f01b2dfec72cc9c7e603a0bf1ca6
rajatbhagat <rajat.bhagat@gmail.com>
2017-07-29 23:11:07 +0530

    Staff table file

--- CFG/MakeAWish/views.py
+++ CFG/MakeAWish/views.py
@@ -7,7 +7,8 @@ from CFG.settings import TEMPLATE_PATH, STATIC_URL
 # Create your views here.
 
 def admin_panel(request):
-    
+    pending_wishes = ChildProfile.objects.get(wish_status = 1 | wish_status = 2 | wish_status = 3)
+    print(pending_wishes)
     return render(request, TEMPLATE_PATH + '/MakeAWish/index.html')
 
 
---
+++ staff_table.sql
@@ -0,0 +1,7 @@
+create table staff (
+staff_id int primary key,
+name varchar(20),
+email varchar(20),
+phone_no varchar(20),
+area varchar(20),
+)
