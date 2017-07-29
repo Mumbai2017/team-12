1a9564f0ef350a1b800d0d61d115dac693674a55
rajatbhagat <rajat.bhagat@gmail.com>
2017-07-29 21:56:06 +0530

    Working Static files

--- CFG/CFG/settings.py
+++ CFG/CFG/settings.py
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(STATIC_URL)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR) + '/MakeAWish/static',
+)
