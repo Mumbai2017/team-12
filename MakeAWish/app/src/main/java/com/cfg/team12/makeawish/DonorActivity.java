ef18ca96d1f25a351c6fe808e95c5e2d60e3f64b
rohitx71 <rohitsingh2359@gmail.com>
2017-07-30 03:27:08 +0530

    doctor along with recycler nd getter setter

--- MakeAWish/app/src/main/java/com/cfg/team12/makeawish/DonorActivity.java
+++ MakeAWish/app/src/main/java/com/cfg/team12/makeawish/DonorActivity.java
@@ -1,7 +1,13 @@
 package com.cfg.team12.makeawish;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DonorActivity extends AppCompatActivity {
 
@@ -9,5 +15,30 @@ public class DonorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor);
+    }
+
+
+
+    class RecyclerViewHolder3 extends RecyclerView.ViewHolder {
+
+        protected TextView txtName, txtAddress;
+        protected ImageView imageView;
+
+        public RecyclerViewHolder3(View itemView, final Context context) {
+            super(itemView);
+            txtName = (TextView) itemView.findViewById(R.id.txt_name);
+
+            txtAddress = (TextView) itemView.findViewById(R.id.txt_address);
+
+
+            RelativeLayout relativeLayout = (RelativeLayout) itemView.findViewById(R.id.rel_list);
+            relativeLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+        }
     }
 }
