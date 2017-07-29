676463b88af7b2a2d1e6a5350e36735d032bf6e7
rohitx71 <rohitsingh2359@gmail.com>
2017-07-30 01:54:59 +0530

    vouchersrecycler

---
+++ MakeAWish/app/src/main/java/com/cfg/team12/makeawish/Vouchers.java
@@ -0,0 +1,114 @@
+package com.cfg.team12.makeawish;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cfg.team12.makeawish.model.ReferredData;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class Vouchers extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private VolunteerDashboardActivity.RecyclerViewAdapter recyclerViewAdapter;
+    ArrayList<ReferredData> arraylist = new ArrayList<>();
+    //String url = "http://freeecommercewebsite.in/Cfg/getvolunteer2.php";
+    int flag = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vouchers);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        ArrayList<ReferredData> data = new ArrayList<>();
+        BigInteger bigInteger = new BigInteger("543534535");
+        ReferredData referredData = new ReferredData("BookMyShow", "Get 20% off!*", bigInteger);
+        data.add(referredData);
+        bigInteger = new BigInteger("543534535");
+        referredData = new ReferredData("BookMyShow", "Get 20% off!*", bigInteger);
+        data.add(referredData);
+        bigInteger = new BigInteger("543534535");
+        referredData = new ReferredData("BookMyShow", "Get 20% off!*", bigInteger);
+        data.add(referredData);
+        bigInteger = new BigInteger("543534535");
+        referredData = new ReferredData("BookMyShow", "Get 20% off!*", bigInteger);
+        data.add(referredData);
+        VolunteerDashboardActivity.RecyclerViewAdapter recyclerViewAdapter = new VolunteerDashboardActivity.RecyclerViewAdapter(data);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setOrientation(linearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    class RecyclerViewAdapter extends RecyclerView.Adapter<VolunteerDashboardActivity.RecyclerViewHolder> {
+
+        ArrayList<ReferredData> referredDatas = new ArrayList<>();
+        //  ArrayList<Bitmap> forecasrArrayList = new ArrayList<>();
+
+
+        RecyclerViewAdapter(ArrayList<ReferredData> referredDataArrayList) {
+            referredDatas = referredDataArrayList;
+
+        }
+
+        @Override
+        public VolunteerDashboardActivity.RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_referral, parent, false);
+            return new VolunteerDashboardActivity.RecyclerViewHolder(view, parent.getContext());
+        }
+
+        @Override
+        public void onBindViewHolder(VolunteerDashboardActivity.RecyclerViewHolder holder, int position) {
+
+            ReferredData rf = referredDatas.get(position);
+            Toast.makeText(VolunteerDashboardActivity.this, "" + rf.getName(), Toast.LENGTH_SHORT).show();
+            holder.txtAddress.setText(referredDatas.get(position).getAddress());
+            holder.txtName.setText(referredDatas.get(position).getAddress());
+            BigInteger number = referredDatas.get(position).getPhone();
+
+
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return referredDatas.size();
+        }
+    }
+
+    class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        protected TextView txtName, txtAddress;
+        protected ImageView imageView;
+
+        public RecyclerViewHolder(View itemView, final Context context) {
+            super(itemView);
+            txtName = (TextView) itemView.findViewById(R.id.txt_name);
+
+            txtAddress = (TextView) itemView.findViewById(R.id.txt_address);
+
+
+            RelativeLayout relativeLayout = (RelativeLayout) itemView.findViewById(R.id.rel_list);
+            relativeLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+        }
+    }
+}
