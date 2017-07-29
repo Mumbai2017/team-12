7bf1451d2e4129947c53afbdcc2338d822aa45eb
rohitx71 <rohitsingh2359@gmail.com>
2017-07-29 22:29:43 +0530

    workingid

--- Php scripts/getchildfromvolid.php
+++ Php scripts/getchildfromvolid.php
@@ -5,14 +5,14 @@ require 'init.php';
 $user_id = "1";
 $date="2016-07-09 01:18:25";
 
-$sql="select * from volunteer where volunteer_id='1';";
+$sql="select * from childprofile where vol_id='1';";
 //echo $sql;
 $res=mysqli_query($con,$sql);
 
 $response=array();
 while($row=mysqli_fetch_assoc($res)){
 
-	array_push($response,array("lat"=>$row['lat'],"lon"=>$row['lon']));
+	array_push($response,array("id"=>$row['id'],"case_no"=>$row['case_no']));
 }
 
 echo json_encode($response);
