6edd6ca65e6fda6d9ee95a572d569a247261c0fe
FraG_OuT <himanshu.totla@gmail.com>
2017-07-30 02:50:27 +0530

    Script and ui updates
    updates

---
+++ MakeAWish/app/src/main/java/com/cfg/team12/makeawish/model/DoctorModel.java
@@ -0,0 +1,16 @@
+package com.cfg.team12.makeawish.model;
+
+/**
+ * Created by Himanshu on 30-07-2017.
+ */
+
+public class DoctorModel {
+
+    String childName;
+    String hospital;
+
+    String status;
+
+
+
+}
