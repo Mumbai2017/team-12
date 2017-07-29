a4ba3dba4ca0e6cabd4e7bc2107da6bb79d53469
rohitx71 <rohitsingh2359@gmail.com>
2017-07-29 15:16:18 +0530

    spinner added

--- MakeAWish/app/src/main/java/com/cfg/team12/makeawish/LoginActivity.java
+++ MakeAWish/app/src/main/java/com/cfg/team12/makeawish/LoginActivity.java
@@ -2,12 +2,25 @@ package com.cfg.team12.makeawish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Spinner stakeholder;
+    String data[]=new String[]{"Donor","Volunteer"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,data);
+
+        stakeholder=(Spinner)findViewById(R.id.spinner_select);
+        stakeholder.setAdapter(arrayAdapter);
+
     }
 }
