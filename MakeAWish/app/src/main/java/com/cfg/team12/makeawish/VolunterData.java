e5bc9961a22d1e35787d0bf0c53020bfa56626ae
rohitx71 <rohitsingh2359@gmail.com>
2017-07-29 19:21:27 +0530

    wishesfieldsupdated

--- MakeAWish/app/src/main/java/com/cfg/team12/makeawish/VolunterData.java
+++ MakeAWish/app/src/main/java/com/cfg/team12/makeawish/VolunterData.java
@@ -2,9 +2,11 @@ package com.cfg.team12.makeawish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.ButterKnife;
 import butterknife.InjectView;
 import info.hoang8f.widget.FButton;
 
@@ -28,5 +30,19 @@ public class VolunterData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunter_data);
+
+        ButterKnife.inject(this);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+    }
+
+    private void register() {
+      
+
     }
 }
