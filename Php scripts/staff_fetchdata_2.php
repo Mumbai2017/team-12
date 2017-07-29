7cd57a22bd0ed6bf4158ad2422e66298a7660e6d
Ria Maheshwari <riamaheshwari2507@gmail.com>
2017-07-30 02:31:12 +0530

    fetch stafff data

---
+++ Php scripts/staff_fetchdata_2.php
@@ -0,0 +1,16 @@
+<?php
+require('init.php');
+
+$sql = "SELECT * from childprofile where status==4"
+$response=array();
+
+if($result=mysqli_query($con,$sql)){
+	while($row=mysqli_fetch_assoc($result)){
+	array_push($response,array("child_name"=>$row['child_name'],"id"=>$row['id'],"hospital"=>$row['hospital'],"contact_no"=>$row['contact_no'],"approved_wish"=>$row['approved_wish'],"status"=>$row['status']),"donor"=>$row['donor_id'],);
+        }
+	}
+echo json_encode($response);
+
+
+
+?>
