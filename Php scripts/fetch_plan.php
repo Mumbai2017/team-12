4494cd6cf5702d89d93a37350081928f419f4cb3
Ria Maheshwari <riamaheshwari2507@gmail.com>
2017-07-30 03:38:29 +0530

    fetch plan php

--- Php scripts/fetch_plan.php
+++ Php scripts/fetch_plan.php
@@ -1,22 +1,17 @@
 <?php
 require 'init.php';
-$_POST['wish'];
+$wish=$_POST['wish'];
 
 $sql="SELECT * from plan where wish like '%".$wish."%'";
 
 $response=array();
 
-if($response=mysqli_query($con,$sql)){
-	$code="reg_success";
-	$message="found plan";
-}else{
-	$message="Not found";
-}
-array_push($response, array("plan-id"=>$response[0]['plan_id'],"plan"=>$response[0]['plan']));
-
+if($result=mysqli_query($con,$sql)){
+	while($row=mysqli_fetch_assoc($result)){
+	array_push($response, array("plan-id"=>$row['plan_id'],"plan"=>$row['plan']));
+	    }
+	}
 echo json_encode($response);
-
 mysqli_close($con);
-
 
 ?>
