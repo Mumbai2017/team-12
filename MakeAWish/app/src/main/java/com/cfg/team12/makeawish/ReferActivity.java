621835197abfbbf07aea6f82e635f8bea883d944
rohitx71 <rohitsingh2359@gmail.com>
2017-07-29 16:32:17 +0530

    referactivity java initilized

--- MakeAWish/app/src/main/java/com/cfg/team12/makeawish/ReferActivity.java
+++ MakeAWish/app/src/main/java/com/cfg/team12/makeawish/ReferActivity.java
@@ -2,12 +2,45 @@ package com.cfg.team12.makeawish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class ReferActivity extends AppCompatActivity {
+
+    @InjectView(com.cfg.team12.makeawish.R.id.input_email)
+    EditText email;
+    @InjectView(R.id.input_phone)
+    EditText phone;
+    @InjectView(R.id.input_address)
+    EditText location;
+
+
+    @InjectView(R.id.btn_login)
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refer);
+
+        ButterKnife.inject(this);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+    }
+
+    private void register() {
+        String email2 = email.getText().toString();
+        String phone2 = phone.getText().toString();
+        String location2 = location.getText().toString();
+
     }
 }
