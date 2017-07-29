f28fb629f309370a677798b1a8ba570ac6d4a918
rajatbhagat <rajat.bhagat@gmail.com>
2017-07-30 04:55:21 +0530

    uygr

--- Php scripts/init1.php
+++ Php scripts/init1.php
@@ -1,6 +1,6 @@
 <?php
 $db="cfg";
-$host="54.254.135.31";
+$host="localhost";
 $password="root";
 $user="force";
 
@@ -13,9 +13,9 @@ $host="localhost";*/
 $con=mysqli_connect($host,$user,$password,$db);
 
 if($con){
-//echo "Connection successful";
+echo "Connection successful";
 }else{
-	//echo "Error in conncetion";
+	echo "Error in conncetion";
 }
 
 ?>
