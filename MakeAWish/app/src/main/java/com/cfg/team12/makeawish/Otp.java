308badce6ec1bd4bba0c66c0f20de05340f87ad2
rohitx71 <rohitsingh2359@gmail.com>
2017-07-30 09:59:10 +0530

    dd

--- MakeAWish/app/src/main/java/com/cfg/team12/makeawish/Otp.java
+++ MakeAWish/app/src/main/java/com/cfg/team12/makeawish/Otp.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Otp extends AppCompatActivity {
 
@@ -13,10 +14,12 @@ public class Otp extends AppCompatActivity {
         setContentView(R.layout.activity_otp);
 
 
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+        Button b = (Button) findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(),VolunteerDashboardActivity.class);
+               // Intent i = new Intent(getApplicationContext(), VolunterData.class);
+                //startActivity();
             }
         });
 
