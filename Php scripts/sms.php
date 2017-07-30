b2f2a6017b959241b500291104c24f718461f50f
Ria Maheshwari <riamaheshwari2507@gmail.com>
2017-07-30 06:32:06 +0530

    sms.php

---
+++ Php scripts/sms.php
@@ -0,0 +1,31 @@
+<?php
+$post_data = array(
+    // 'From' doesn't matter; For transactional, this will be replaced with your SenderId;
+    // For promotional, this will be ignored by the SMS gateway
+    'From'   => '+918039513592',
+    'To'    => array('918369118261', '+919769259947'),
+    'Body'  => 'Hi John, your number 9123456780 is now turned NEW WISH RECIEVED.'
+);
+
+$exotel_sid = "sp597"; // Your Exotel SID
+$exotel_token = "9b590f414c114a62dcead7ff249a5befd6acad30"; // Your exotel token
+  
+$url = "https://".$exotel_sid.":".$exotel_token."@twilix.exotel.in/v1/Accounts/".$exotel_sid."/Sms/send";
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+ 
+$http_result = curl_exec($ch);
+$error = curl_error($ch);
+$http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
+ 
+curl_close($ch);
+ 
+print "Response = ".print_r($http_result);
+?>
